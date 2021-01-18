81119d260a575c5c2da35f6a06b787300d9e2535
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-01-18 12:32:02 -0800

    Add requirements, auto install requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='jakesutils',
-      version='0.2',
-      description='A collection of utils that I use all the time in my projects',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/JakeRoggenbuck/jakesutils',
-      author='Jake Roggenbuck',
-      author_email='jake@jr0.org',
-      license='MIT',
-      packages=['jakesutils'],
-      zip_safe=False)
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='jakesutils',
+    version='0.3',
+    description='A collection of utils that I use all the time in my projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JakeRoggenbuck/jakesutils',
+    author='Jake Roggenbuck',
+    author_email='jake@jr0.org',
+    license='MIT',
+    packages=['jakesutils'],
+    install_requires=required,
+    zip_safe=False,
+)
